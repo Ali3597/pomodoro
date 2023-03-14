89e2a28883846ad84effc2d11cf2d4fef34dd571
Ali <ali_saleh-4@hotmail.fr>
2023-03-14 10:09:47 +0100

    alter task model and validation schema

--- server/src/database/models/task.model.ts
+++ server/src/database/models/task.model.ts
@@ -1,11 +1,13 @@
 import  mongoose from 'mongoose';
-
+const schema = mongoose.Schema;
 
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     details : {type : String},
-    created_sat: {type: Date,require: true,default: Date.now()},
-    done_at:{type: Date , required:false, default: null}
+    created_at: {type: Date,require: true,default: Date.now()},
+    updated_at: {type: Date,require: false},
+    done_at:{type: Date , required:false, default: null},
+    author: { type: schema.Types.ObjectId, ref: "user", required: true },
 });
 
 taskSchema.add({ subTasks: [taskSchema] })
---
+++ server/src/database/validation/task.validation.ts
@@ -0,0 +1,19 @@
+import  Joi from "joi";
+
+
+const taskInfoValidation = Joi.object({
+    title: Joi.string().min(3).required().messages({
+        "string.base": "Le  titre n'est pas valide",
+        "string.empty": "Vous n' avez pas de titre ",
+        "sting.min": "Votre titre est trop court",
+    }),
+    details: Joi.string().min(5).messages({
+      "string.base": "Les details  ne sont  pas valide",
+      "sting.min": "Votre  details  sont  trop court",
+    }),
+  });
+
+  export  {
+    taskInfoValidation,
+    
+  };
