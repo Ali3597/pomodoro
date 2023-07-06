008618947346bfb24ff17985ccf2a4eab52f2e3c
Ali Saleh <ali_saleh-4@hotmail.fr>
2023-07-06 22:44:13 +0200

    feat: prod env docker

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,8 +1,12 @@
 import  mongoose from 'mongoose';
+
+console.log(process.env.MONGO_USERNAME, "process.env.MONGO_USERNAME","il sont la");
+console.log(process.env.MONGO_PWD, "process.env.MONGO_PWD","il sont la");
+console.log(process.env.NODE_ENV,"voila mon environnement de node");
 const uri = process.env.NODE_ENV === 'production' ?
 `mongodb://${ process.env.MONGO_USERNAME }:${ process.env.MONGO_PWD }@db` :
 `mongodb://db`;
-
+console.log(uri,"voilalalallaallalalalalallalalalal mon uriii");
 mongoose.connect( uri).then(() => {
     console.log('Connected !')
 }).catch(e => console.log(e));
